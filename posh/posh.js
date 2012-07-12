4310cd005806484c814eba7bcd24603e9496fd55
Eric Cornelius <EricMCornelius@gmail.com>
2012-07-12 11:35:21 -0400

    Temp

--- posh/posh.js
+++ posh/posh.js
@@ -6,7 +6,7 @@ var util = require('util');
 var proc = require('child_process');
 var async = require('async');
 
-var DEBUG = false;
+var DEBUG = true;
 
 dump = function(arg) {
   if (!DEBUG)
@@ -24,14 +24,13 @@ fs.copy = function (src, dst, cb) {
   function copy(err) {
     var is, os;
 
-    if (!err) {
-      return cb(new Error("File " + dst + " exists."));
-    }
+    if (!err)
+      console.log('Overwriting file: ' + dst);
 
     fs.stat(src, function (err) {
-      if (err) {
+      if (err)
         return cb(err);
-      }
+
       is = fs.createReadStream(src);
       os = fs.createWriteStream(dst);
       util.pump(is, os, cb);
@@ -59,8 +58,35 @@ function initialize(obj, vars) {
   }
 }
 
+function launch(args, cb) {
+  dump(args);
+
+  var invoke = proc.spawn(args.cmd, args.args);
+  invoke.stdout.on('data', function(data) {
+    dump(data);
+  });
+
+  invoke.stderr.on('data', function(data) {
+    console.log(data + '');
+  });
+
+  invoke.on('exit', function(code) {
+    if (code !== 0)
+      return cb(new Error(code));
+
+    cb(null);
+  });
+};
+
 function BuildSetType(vars) {
-  initialize(this, vars);
+  var self = this;
+  initialize(self, vars);
+
+  self.topological_order = [];
+  self.name_to_project_map =  {};
+  self.add_project = self.add_project.bind(self);
+  self.lookup_project = self.lookup_project.bind(self);
+  self.topological_sort = self.topological_sort.bind(self);
 };
 
 BuildSetType.prototype.defaults = {
@@ -73,16 +99,16 @@ BuildSetType.prototype.defaults = {
   },
   topological_order: [],
   name_to_project_map: {}
-}
+};
 
 BuildSetType.prototype.add_project = function(project) {
   this.projects.push(project);
   this.name_to_project_map[project.name] = project;
-}
+};
 
 BuildSetType.prototype.lookup_project = function(name) {
   return this.name_to_project_map[name];
-}
+};
 
 BuildSetType.prototype.topological_sort = function() {
   var ordered = [];
@@ -136,10 +162,7 @@ BuildSetType.prototype.topological_sort = function() {
 
   visit('root', {});
 
-  this.topological_order = ordered.reverse();
-
-  // remove root
-  this.topological_order.shift();
+  this.topological_order = ordered.filter(function(name) { return name !== 'root'; });
 };
 
 BuildSetType.prototype.generate_dependency_graph = function() {
@@ -172,6 +195,25 @@ BuildSetType.prototype.generate_dependency_graph = function() {
   this.topological_sort();
 };
 
+BuildSetType.prototype.generate_transitive_closure = function(project) {
+  var projects = project.deps.splice(0);
+  var closure = {};
+
+  while (projects.length > 0) {
+    var p = this.lookup_project(projects.shift());
+    closure[p.name] = p;
+    projects = projects.concat(p.deps);
+  }
+
+  var ordered = [];
+  this.topological_order.forEach(function(name) {
+    if (name in closure)
+      ordered.push(name);
+  });
+
+  return ordered.map(this.lookup_project);
+};
+
 BuildSet = function(vars) {
   return new BuildSetType(vars);
 };
@@ -180,68 +222,67 @@ GlobalBuildSet = BuildSet({name: "GlobalBuildSet"});
 
 ProjectType = function(vars) {
   var self = this;
+  initialize(self, vars);
 
-  self.name = "";
-  self.type = "";
-  self.target = "";
-  self.deps = [];
-  self.files = [];
-  self.headers = [];
-  self.resources = [];
-  self.options = [];
-  self.prebuild = function() { };
-  self.compile = function() { };
-  self.link = function() { }; 
-  self.install = function() { };
-  self.postbuild = function() { };
-  self.__prebuild = function(cb) { self.prebuild(); cb(null); };
-  self.__compile = function(cb) { self.toolchain.compile(self, cb); };
-  self.__link = function(cb) { self.toolchain.link(self, cb); };
-  self.__install = function(cb) { self.toolchain.install(self, cb); };
-  self.__postbuild = function(cb) { self.postbuild(); cb(null); };
+  self.__prebuild = self.__prebuild.bind(self);
+  self.__compile = self.__compile.bind(self);
+  self.__link = self.__link.bind(self);
+  self.__publish = self.__publish.bind(self);
+  self.__postbuild = self.__postbuild.bind(self);
 
   self.build_set = GlobalBuildSet;
   self.toolchain = null;
-
-  initialize(self, vars);
   self.build_set.add_project(self);
 
-  if (!('root' in vars))
+  if (!exists(vars.root))
     self.root = path.dirname(current_file);
 
-  if (!('output_root' in vars))
+  if (!exists(vars.output_root))
     self.output_root = self.root;
 
-  if (!('outputdir' in vars))
-    self.outputdir = path.join(self.output_root, 'build');
-  else
-    self.outputdir = path.join(self.output_root, self.outputdir);
-
-  if (!('bindir' in vars))
-    self.bindir = path.join(self.output_root, 'bin');
-  else
-    self.bindir = path.join(self.output_root, self.bindir);
-
-  if (!('libdir' in vars))
-    self.libdir = path.join(self.output_root, 'lib');
-  else
-    self.libdir = path.join(self.output_root, self.libdir);
-
-  if (!('includedir' in vars))
-    self.includedir = path.join(self.output_root, 'include');
-  else
-    self.includedir = path.join(self.output_root, self.includedir);
-
-  if (!('resourcedir' in vars))
-    self.resourcedir = path.join(self.output_root, 'resources');
-  else
-    self.resourcedir = path.join(self.output_root, self.resourcedir);
-
-  if (!('toolchain' in vars))
+  if (!exists(vars.toolchain))
     self.toolchain = current_toolchain;
 
-  if (!('target' in vars))
+  if (!exists(vars.target))
     self.target = self.name;
+
+  var default_dirs = [
+    ['outputdir', 'build'],
+    ['bindir', 'bin'],
+    ['libdir', 'lib'],
+    ['includedir', 'include'],
+    ['resourcedir', 'resources']
+  ];
+
+  default_dirs.forEach(function(info) {
+    var field = info[0];
+    var def = info[1];
+    if (exists(vars[field]))
+      self[field] = path.join(self.output_root, self[field]);
+    else
+      self[field] = path.join(self.output_root, def);
+  });
+};
+
+ProjectType.prototype.defaults = {
+  name: "",
+  type: "",
+  target: "",
+  deps: [],
+  files: [],
+  headers: [],
+  resources: [],
+  options: [],
+  prebuild: function() { },
+  compile: function() { },
+  link: function() { },
+  publish: function() { },
+  postbuild: function() { },
+  __prebuild: function(cb) { this.prebuild(); cb(null); },
+  __compile: function(cb) { this.toolchain.compile(this, cb); },
+  __link: function(cb) { this.toolchain.link(this, cb); },
+  __publish: function(cb) { this.toolchain.publish(this, cb); },
+  __postbuild: function(cb) { this.postbuild(); cb(null); }
 };
 
 Project = function(vars) {
@@ -267,13 +308,19 @@ BuildSettings = function(vars) {
 
 GccToolchainType = function(args) {
   var self = this;  
+  initialize(self, args);
 
-  self.name = "g++",
-  self.compiler = "g++",
-  self.linker = "g++",
-  self.settings = BuildSettings({include_paths: ['include']}),
+  this.compile = this.compile.bind(self);
+  this.compile_env = this.compile_env.bind(self);
+};
 
-  self.generators = {
+GccToolchainType.prototype.defaults = {
+  name: "g++",
+  compiler: "g++",
+  linker: "g++",
+  settings: BuildSettings({include_paths: ['include']}),
+
+  generators: {
     includes: function(args) {
       return args.map(function(path) {
         return '-I' + path;
@@ -312,189 +359,162 @@ GccToolchainType = function(args) {
       return args;
     },
 
-    append_settings: function(settings, appended) {
+    collate_settings: function(obj1, obj2) {
       var fields = ['include_paths', 'lib_paths', 'libs', 'compiler_flags', 'linker_flags', 'defines'];
-      fields.map(function(field) {
-        settings[field].concat(appended[field]);
+      return fields.map(function(field) {
+        return obj1[field].concat(obj2[field]);
       });
     },
-  };
+  },
+};
 
-  self.compile_env = function(project) {
-    var settings = BuildSettings(project);
-   
-    var deps = project.deps.map(function(name) { return GlobalBuildSet.lookup_project(name); });
-    deps.forEach(function(dep) { 
-      settings.include_paths.push(dep.includedir); 
-    });
+GccToolchainType.prototype.compile_env = function(project) {
+  //var settings = BuildSettings(project);
+  var settings = this.collate_settings(project, this.settings);
 
-    self.generators.append_settings(settings, self.settings);
+  var deps = GlobalBuildSet.generate_transitive_closure(project);
+  deps.forEach(function(dep) {
+    settings.include_paths.push(dep.includedir);
+  });
  
-    var flags = self.generators.compiler_flags(settings.compiler_flags);
-    var defines = self.generators.defines(settings.defines);
-    var includes = self.generators.includes(settings.include_paths);
+  var flags = this.generators.compiler_flags(settings.compiler_flags);
+  var defines = this.generators.defines(settings.defines);
+  var includes = this.generators.includes(settings.include_paths);
 
-    var args = flags.concat(defines).concat(includes);
+  var args = flags.concat(defines).concat(includes);
 
+  return {
+    compiler: this.compiler,
+    args: args
+  }
+};
+  
+GccToolchainType.prototype.link_env = function(project) {
+  //var settings = BuildSettings(project);
+  
+  var deps = GlobalBuildSet.generate_transitive_closure(project);
+  deps.forEach(function(dep) {
+    settings.lib_paths.push(dep.libdir);
+    settings.libs.unshift(dep.target);
+  });
+  
+  var flags = this.generators.linker_flags(settings.linker_flags);
+  var libpaths = this.generators.libs(settings.lib_paths);
+  var links = this.generators.links(settings.libs);
+  var rpaths = this.generators.rpaths(settings.lib_paths);
+
+  var args = flags.concat(libpaths).concat(links).concat(rpaths);
+
+  return {
+    linker: this.linker,
+    args: args
+  }
+};
+
+GccToolchainType.prototype.publish_env = function(project) {
+  return { };
+};
+
+GccToolchainType.prototype.compile = function(project, cb) {
+  console.log('Compiling ' + project.name + '...');
+  var env = this.compile_env(project);
+  
+  try {
+    fs.mkdirSync(project.outputdir);
+  }
+  catch(e) { } 
+
+  var files = [];
+  project.files.map(function(file) {
+    var file_path = path.join(project.root, file);
+    files = files.concat(glob.sync(file_path, null));
+  });
+
+  var commands = files.map(function(file) {
+    var base = path.basename(file, path.extname(file));
+    output = path.join(project.outputdir, base + '.o');
+    args = env.args.concat(['-c', file, '-o', output]);
     return {
-      compiler: self.compiler,
-      args: args
-    }
-  };
-  
-  self.link_env = function(project) {
-    var settings = BuildSettings(project);
-  
-    var deps = project.deps.map(function(name) { return GlobalBuildSet.lookup_project(name); });
-    deps.forEach(function(dep) {
-      settings.lib_paths.push(dep.libdir);
-      settings.libs.unshift(dep.target);
-    });
+      cmd: env.compiler,
+      args: args,
+    };
+  });
 
-    self.generators.append_settings(settings, self.settings);
-  
-    var flags = self.generators.linker_flags(settings.linker_flags);
-    var libpaths = self.generators.libs(settings.lib_paths);
-    var links = self.generators.links(settings.libs);
-    var rpaths = self.generators.rpaths(settings.lib_paths);
+  async.forEach(
+    commands,
+    function(cmd, cb) { launch(cmd, cb); },
+    function(err) { cb(null); }
+  );
+};
 
-    var args = flags.concat(libpaths).concat(links).concat(rpaths);
+GccToolchainType.prototype.publish = function(project, cb) {
+  console.log('Publishing ' + project.name + '...');
+  var env = this.publish_env(project);
+    
+  try {
+    fs.mkdirSync(project.includedir);
+  }
+  catch(e) { }
 
-    return {
-      linker: self.linker,
-      args: args
-    }
-  };
+  var files = [];
+  project.headers.map(function(file) {
+    files = files.concat(glob.sync(file, {cwd: project.root}));
+  });
 
-  self.install_env = function(project) {
-    return { };
+  async.forEach(
+    files, 
+    function(file, cb) { fs.copy(path.join(project.root, file), path.join(project.includedir, file), cb); },
+    function(err) { cb(null); }
+  );
+};
+
+GccToolchainType.prototype.link = function(project, cb) {
+  console.log('Linking ' + project.name + '...');
+  var env = this.link_env(project);
+
+  try {
+    fs.mkdirSync(project.libdir);
+  }
+  catch(e) { } 
+
+  try {
+    fs.mkdirSync(project.bindir);
+  }
+  catch(e) { } 
+    
+  var obj_glob = path.join(project.outputdir, '*.o');
+  var files = glob.sync(obj_glob, null);
+
+  args = [].concat(env.args);
+    
+  var target = project.target;
+  var output = '';
+  if (project.type === 'SharedLib') {
+    args = args.concat('-shared');
+    target = 'lib' + target + '.so';
+    output = path.join(project.libdir, target);
+  }
+  else if(project.type === 'StaticLib') {
+    target = 'lib' + target + '.a';
+    output = path.join(project.libdir, target);
+    args = ['rcs', output];
+    env.linker = 'ar';
+  }
+  else if(project.type === 'Application') {
+    output = path.join(project.bindir, target);
   }
 
-  self.install = function(project, cb) {
-    console.log('Installing ' + project.name + '...');
-    var env = self.install_env(project);
-    
-    try {
-      fs.mkdirSync(project.includedir);
-    }
-    catch(e) { }
+  if (project.type !== 'StaticLib')
+    args = args.concat('-o', output);
 
-    var files = [];
-    project.headers.map(function(file) {
-      files = files.concat(glob.sync(file, {cwd: project.root}));
-    });
-
-    async.forEach(
-      files, 
-      function(file, cb) { fs.copy(path.join(project.root, file), path.join(project.includedir, file), cb); },
-      function(err) { cb(null); }
-    );
-  };
-
-  self.compile = function(project, cb) {
-    console.log('Compiling ' + project.name + '...');
-    var env = self.compile_env(project);
+  args = args.concat(files);
   
-    try {
-      fs.mkdirSync(project.outputdir);
-    }
-    catch(e) { } 
-
-    var files = [];
-    project.files.map(function(file) {
-      var file_path = path.join(project.root, file);
-      files = files.concat(glob.sync(file_path, null));
-    });
-
-    var pending = files.length;
-    files.map(function(file) {
-      var base = path.basename(file, path.extname(file));
-      output = path.join(project.outputdir, base + '.o');
-      args = env.args.concat(['-c', file, '-o', output]);
-
-      dump(env.compiler);
-      dump(args);
-
-      var invoke = proc.spawn(env.compiler, args);
-      invoke.stdout.on('data', function(data) {
-        dump(data);
-      });
-
-      invoke.stderr.on('data', function(data) {
-        console.log(data + '');
-      });
-
-      invoke.on('exit', function(code) {
-        if (code !== 0)
-          return cb(new Error(code));
-        
-        --pending;
-        if (pending === 0)
-          cb(null);
-      });
-    });
+  var command = {
+    cmd: env.linker,
+    args: args
   };
 
-  self.link = function(project, cb) {
-    console.log('Linking ' + project.name + '...');
-    var env = self.link_env(project);
-
-    try {
-      fs.mkdirSync(project.libdir);
-    }
-    catch(e) { } 
-
-    try {
-      fs.mkdirSync(project.bindir);
-    }
-    catch(e) { } 
-    
-    var obj_glob = path.join(project.outputdir, '*.o');
-    var files = glob.sync(obj_glob, null);
-
-    args = [].concat(env.args);
-    
-    var target = project.target;
-    var output = '';
-    if (project.type === 'SharedLib') {
-      args = args.concat('-shared');
-      target = 'lib' + target + '.so';
-      output = path.join(project.libdir, target);
-    }
-    else if(project.type === 'StaticLib') {
-      target = 'lib' + target + '.a';
-      output = path.join(project.libdir, target);
-      args = ['rcs', output];
-      env.linker = 'ar';
-    }
-    else if(project.type === 'Application') {
-      output = path.join(project.bindir, target);
-    }
-
-    if (project.type !== 'StaticLib')
-      args = args.concat('-o', output);
-
-    args = args.concat(files);
-
-    dump(env.linker);
-    dump(args);
-
-    var invoke = proc.spawn(env.linker, args);
-    invoke.stdout.on('data', function(data) {
-      dump(data);
-    });
-
-    invoke.stderr.on('data', function(data) {
-      console.log(data + '');
-    });
-
-    invoke.on('exit', function(code) {
-      if (code !== 0)
-        return cb(new Error(code));
-
-      cb(null);
-    });
-  };
+  launch(command, cb);
 };
 
 current_toolchain = new GccToolchainType();
@@ -512,20 +532,10 @@ var generate = function(files) {
 }
 
 var build = function() {
-  // call each project in the build set in topological order
-  async.mapSeries(GlobalBuildSet.topological_order, function(name, cb) {
-    // retrieve project and chain steps
-    var project = GlobalBuildSet.lookup_project(name);
-    console.log('\n' + name + ' started');
+  var build_glob = '**/build.js'
+  var files = glob.sync(build_glob, null);
+  files = files.map(function(file){ return path.join(path.dirname(__filename), file); });
 
-    async.series([project.__prebuild, project.__compile, project.__link, project.__install, project.__postbuild], 
-      function(err, results) { 
-        if (err) { console.log(err); return cb(err); } console.log(name + ' completed!'); cb(); 
-    });
-  }, function() { });
-}
-
-var post_search = function(err) {
   generate(files);
 
   try {
@@ -537,10 +547,56 @@ var post_search = function(err) {
     console.log(e.message);
   }
 
-  build();
+  var projects = GlobalBuildSet.topological_order.map(function(name) {
+    return GlobalBuildSet.lookup_project(name);
+  });
+
+  var compile = function(err, cb) {
+    async.forEach(
+      projects,
+      function(project, cb) { 
+        console.log('\n' + name + ' started');
+        async.series([project.__prebuild, project.__publish, project.__compile], cb);
+      },
+      function(err) { cb(null); }
+    );
+  };
+
+  var link = function(err) {
+    async.mapSeries(
+      projects,
+      function(project, cb) {
+        async.series([project.__link, project.__postbuild], cb);
+      },
+      function(err) { console.log('Finished'); }
+    );
+  };
+
+  compile(null, link);
 }
 
-var build_glob = '**/build.js'
-var files = glob.sync(build_glob, null);
-files = files.map(function(file){ return path.join(path.dirname(__filename), file); });
-post_search();
+command_map = {
+  'help': {
+    triggers: ['-h', '--h', '-help', '--help', 'help'],
+    action: function() { 
+      console.log('PoshJS C++ Build System'); 
+    }
+  },
+  'build': {
+    triggers: ['-b', '--b', '-build', '--build', 'build'],
+    action: function() {
+      build();
+    }    
+  }
+};
+
+process.argv.forEach(function(arg) {
+  for (name in command_map) {
+    var command = command_map[name];
+    command.triggers.forEach(function(trigger) {
+      if (trigger === arg) {
+        command.action();
+      }
+    });
+  }
+});
