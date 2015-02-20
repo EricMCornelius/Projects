65c05ed76369183f7b73637976d781af1c9da690
Eric Cornelius <EricMCornelius@gmail.com>
2015-02-20 07:41:12 -0500

    Added fptr signature typedef

--- template_metaprogramming/fptrs/src/main.cpp
+++ template_metaprogramming/fptrs/src/main.cpp
@@ -11,6 +11,16 @@ struct SigType<R(Args...)> {
   using args = typename std::tuple_element<N, std::tuple<Args...>>::type;
 
   typedef R result;
+  typedef R(*fptr)(Args...);
+};
+
+template <typename R, typename... Args>
+struct SigType<R(*)(Args...)> {
+  template <std::size_t N>
+  using args = typename std::tuple_element<N, std::tuple<Args...>>::type;
+
+  typedef R result;
+  typedef R(*fptr)(Args...);
 };
 
 template <typename R, typename C, typename... Args>
@@ -19,6 +29,7 @@ struct SigType<R (C::*)(Args...)> {
   using args = typename std::tuple_element<N, std::tuple<Args...>>::type;
 
   typedef R result;
+  typedef R(*fptr)(Args...);
 };
 
 struct Test {
@@ -35,6 +46,10 @@ int fp_test(int, float) {
   return 0;
 }
 
+void test_func(bool) {
+  
+}
+
 int main(int argc, char* argv[]) {
   typedef typename SigType<decltype(fp_test)>::args<0> int_type;
   typedef typename SigType<decltype(fp_test)>::args<1> float_type;
@@ -45,4 +60,10 @@ int main(int argc, char* argv[]) {
   static_assert(std::is_same<double_type, double>::value, "must be double");
   typedef typename SigType<decltype(&Test::operator())>::args<0> string_type;
   static_assert(std::is_same<string_type, std::string>::value, "must be string");
+
+  typedef typename SigType<void(*)(long)>::args<0> long_type;
+  static_assert(std::is_same<long_type, long>::value, "must be long");
+
+  typedef SigType<void(*)(bool)> bool_sig;
+  typename bool_sig::fptr tmp = &test_func;
 }
