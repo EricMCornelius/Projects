0c4bdb67b78b21441b89721c6e59f6634558ebd0
Eric Cornelius <EricMCornelius@gmail.com>
2015-02-20 08:32:49 -0500

    Lambda introspection

--- template_metaprogramming/fptrs/src/main.cpp
+++ template_metaprogramming/fptrs/src/main.cpp
@@ -2,7 +2,7 @@
 #include <type_traits>
 #include <tuple>
 
-template <typename... Args>
+template <typename T>
 struct SigType;
 
 template <typename R, typename... Args>
@@ -10,6 +10,7 @@ struct SigType<R(Args...)> {
   template <std::size_t N>
   using args = typename std::tuple_element<N, std::tuple<Args...>>::type;
 
+  typedef std::tuple<Args...> tuple;
   typedef R result;
   typedef R(*fptr)(Args...);
 };
@@ -19,6 +20,7 @@ struct SigType<R(*)(Args...)> {
   template <std::size_t N>
   using args = typename std::tuple_element<N, std::tuple<Args...>>::type;
 
+  typedef std::tuple<Args...> tuple;
   typedef R result;
   typedef R(*fptr)(Args...);
 };
@@ -28,9 +30,33 @@ struct SigType<R (C::*)(Args...)> {
   template <std::size_t N>
   using args = typename std::tuple_element<N, std::tuple<Args...>>::type;
 
+  typedef std::tuple<Args...> tuple;
   typedef R result;
   typedef R(*fptr)(Args...);
 };
+
+template <typename R, typename C, typename... Args>
+struct SigType<R (C::*)(Args...) const> {
+  template <std::size_t N>
+  using args = typename std::tuple_element<N, std::tuple<Args...>>::type;
+
+  typedef std::tuple<Args...> tuple;
+  typedef R result;
+  typedef R(*fptr)(Args...);
+};
+
+template <typename T>
+struct SigType {
+  typedef SigType<decltype(&T::operator())> internal;
+
+  template <std::size_t N>
+  using args = typename std::tuple_element<N, typename internal::tuple>::type;
+
+  typedef typename internal::result result;
+  typedef typename internal::fptr fptr;
+};
+
+
 
 struct Test {
   bool operator()(std::string) {
@@ -47,7 +73,17 @@ int fp_test(int, float) {
 }
 
 void test_func(bool) {
-  
+
+}
+
+enum class test_enum : uint16_t {
+  ONE = 1,
+  TWO,
+  THREE
+};
+
+test_enum test_func2(int, double, void*) {
+  return test_enum::ONE;
 }
 
 int main(int argc, char* argv[]) {
@@ -66,4 +102,15 @@ int main(int argc, char* argv[]) {
 
   typedef SigType<void(*)(bool)> bool_sig;
   typename bool_sig::fptr tmp = &test_func;
+
+  auto lambda_ex = [](int a, double b) -> decltype(a) { return a; };
+  typedef SigType<decltype(lambda_ex)> lambda_sig;
+  typedef typename lambda_sig::args<0> lambda_1;
+  static_assert(std::is_same<lambda_1, int>::value, "must be same");
+
+  typedef SigType<enum test_enum(int, double, void*)> complicated_sig;
+  typename complicated_sig::fptr tmp2 = &test_func2;
+
+  std::cout << sizeof(tmp2(0,0,nullptr)) << std::endl;
+  std::cout << static_cast<int>(tmp2(0,0,nullptr)) << std::endl;
 }
